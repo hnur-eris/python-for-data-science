528009e40d5d806ae4241fdc8b61262823d329df
hnur-eris <erisshatice6@gmail.com>
2024-08-24 16:37:50 +0300

    Fix null structure but I have a mistake about float data type

--- ex03/NULL_not_found.py
+++ ex03/NULL_not_found.py
@@ -2,21 +2,24 @@ no_names = {
     "NoneType": "Nothing",
     "float": "Cheese",
     "int": "Zero",
-    "str": "Empty",
-    "bool": "Fake"
+    "bool": "Fake",
+    "str": "Empty"
 }
 
 def NULL_not_found(object: any) -> int:
-
-    class_name = object.__class__.__name__
-    if object != "Brian":
-        print(f"{print_names(class_name)}: {object} {object.__class__}")
-        return 0
+    null = [None, "", '', float("NaN"), 0, False ]
+    if object in null:
+        class_name = object.__class__.__name__
+        if class_name in no_names:
+            print(f"{print_names(class_name)}: {object} {object.__class__}")
+            return 0
     else:
-        print(f"Type not found")
+        print("Type not Found")  
         return 1
 
 def print_names(class_name: str) -> str:
     return no_names.get(class_name)
 
-# NULL_not_found("merhaba")
+#float kısmında type not found basıyo
+#bunun haricinde doğru çalışıyo
+
