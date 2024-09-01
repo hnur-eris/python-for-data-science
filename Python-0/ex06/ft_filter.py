b8edd70ce497e51dc5f68f62b58dc6f2a3f67c01
hnur-eris <erisshatice6@gmail.com>
2024-09-01 13:16:44 +0300

    fix filter function

--- Python-0/ex06/ft_filter.py
+++ Python-0/ex06/ft_filter.py
@@ -1,8 +1,20 @@
 def ft_filter(function, iterator):
+    """
+    filter.__doc__
+    Return an iterator yielding those items of
+    iterable for which function(item)
+    is true. If function is None,
+    return the items that are true.
+
+    if function is none, the function lists
+    the elements that evaluate to true
+    that means function equals bool.
+    else if every elemnst in list is true,
+    return an iterator.
+    """
     if function is None:
-        # function = bool
         function = bool
-        
+
     for i in iterator:
         if function(i):
             yield i
