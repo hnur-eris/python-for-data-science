daf4f015af85295bd320abb6d55625248a000dc8
hnur-eris <erisshatice6@gmail.com>
2024-09-05 14:27:08 +0300

    added control method in function parameter

--- Python-1/ex00/give_bmi.py
+++ Python-1/ex00/give_bmi.py
@@ -1,8 +1,31 @@
 import numpy as np
 
 
+def control(height: list, weight: list):
+    if height.__class__ .__name__ != "list" or \
+         weight.__class__ .__name__ != "list":
+        raise ValueError("Both height and weight must be lists.")
+    if not all(isinstance(h, (int, float)) for h in height):
+        raise TypeError("All height values must be integers or floats.")
+
+    if not all(isinstance(w, (int, float)) for w in weight):
+        raise TypeError("All weight values must be integers or floats.")
+
+    if len(height) != len(weight):
+        raise ValueError("Height and weight lists must be of the same length.")
+
+
 def give_bmi(height: list[int | float],
              weight: list[int | float]) -> list[int | float]:
+    try:
+        control(height, weight)
+    except TypeError as e:
+        print(e)
+        exit()
+    except ValueError as e:
+        print(e)
+        exit()
+
     h_arr = np.array(height)
     w_arr = np.array(weight)
     bmi_value = w_arr / h_arr ** 2
@@ -10,10 +33,13 @@ def give_bmi(height: list[int | float],
 
 
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
-    if not all(isinstance(b, (int, float)) for b in bmi):
-        raise TypeError("All BMI values must be integers or floats.")
+    try:
+        if not all(isinstance(b, (int, float)) for b in bmi):
+            raise TypeError("All BMI values must be integers or floats.")
 
-    if not isinstance(limit, int):
-        raise TypeError("Limit must be an integer.")
-
+        if not isinstance(limit, int):
+            raise TypeError("Limit must be an integer.")
+    except TypeError as e:
+        print(e)
+        exit()
     return [b > limit for b in bmi]
