6c59155c2f787b3382268ca6d0abe4e70c68accd
hnur-eris <erisshatice6@gmail.com>
2024-09-05 15:41:29 +0300

    added try except statement

--- Python-1/ex01/array2D.py
+++ Python-1/ex01/array2D.py
@@ -2,6 +2,15 @@ import numpy as np
 
 
 def slice_me(family: list, start: int, end: int) -> list:
+    try:
+        if not isinstance(family, list) \
+             or not isinstance(start, int) or not isinstance(end, int):
+            raise AssertionError("Input must be a list and 2 integer")
+        if not all(len(i) == len(family[0]) for i in family):
+            raise AssertionError("All elements must have the same length")
+    except AssertionError as e:
+        print(e)
+        exit()
     arr = np.array(family)
     print(f"My shape is : {arr.shape}")
     print(f"My new shape is : {arr[start:end].shape}")
