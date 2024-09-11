432c19656682e04fd2721491bd3f245fb5b7d585
hnur-eris <erisshatice6@gmail.com>
2024-09-11 21:20:28 +0300

    okki ex03

--- Python-0/ex03/NULL_not_found.py
+++ Python-0/ex03/NULL_not_found.py
@@ -7,12 +7,12 @@ no_names = {
 }
 
 def NULL_not_found(object: any) -> int:
-    null = [None, "", '', float("NaN"), 0, False ]
-    if object in null:
-        class_name = object.__class__.__name__
-        if class_name in no_names:
-            print(f"{print_names(class_name)}: {object} {object.__class__}")
-            return 0
+    null = [None, "", '', float("NaN"), 0, False,"nan"]
+    
+    if object in null or object != object:
+        class_name = object.__class__.__name__            
+        print(f"{print_names(class_name)}: {object} {object.__class__}")
+        return 0
     else:
         print("Type not Found")  
         return 1
@@ -20,6 +20,4 @@ def NULL_not_found(object: any) -> int:
 def print_names(class_name: str) -> str:
     return no_names.get(class_name)
 
-#float kısmında type not found basıyo
-#bunun haricinde doğru çalışıyo
 #tester.py silmeyi unutma
