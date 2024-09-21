eeb2c1e96ef3fd2c4bf7bbfccdd97ea2863f4cad
hnur-eris <erisshatice6@gmail.com>
2024-09-21 16:04:23 +0300

    updated ex04

--- Python-0/ex04/whatis.py
+++ Python-0/ex04/whatis.py
@@ -4,9 +4,9 @@ ac = sys.argv
 try:
     if len(ac) == 1:
         exit(0) 
-    assert len(ac) <= 3, "more than one argument is provided"
+    assert len(ac) < 3, "more than one argument is provided"
     data = sys.argv[1]
-    assert data.lstrip("-").isnumeric(), "argument is not an integer"
+    assert data.lstrip("-+").isnumeric(), "argument is not an integer"
     number = int(data)
     print("I'm Even.") if number % 2 == 0 else print("I'm Odd.")
 except AssertionError as msg:
