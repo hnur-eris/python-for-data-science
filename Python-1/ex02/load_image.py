5a03f062e6086bc09f13c6fe996b3ac121da2aac
hnur-eris <erisshatice6@gmail.com>
2024-09-24 14:09:19 +0300

    added error function

--- Python-1/ex02/load_image.py
+++ Python-1/ex02/load_image.py
@@ -4,18 +4,18 @@ from numpy import array
 import os
 
 
+def error_handle(path: str):
+    assert not path.endswith(("jpg", "jpeg")), "Doesn't match expected format"
+    assert not os.path.exists(path), "File doesn't found"
+
+
 def ft_load(path: str) -> array:
     try:
-        assert not path.endswith(("jpg", "jpeg")), "The file \
-            xtension does not match the expected \
-                format such as JPG or JPEG"
-        assert not os.path.exists(path), "File doesn't found"
+        error_handle(path)
     except AssertionError as e:
         print(f"AssertionError: {e}")
         exit()
     im = Image.open(path)
-    im_arr = np.array(im)
     width, height = im.size
-    print(f"The shape of image is ({height}, {width}, \
-        {len(im.getbands())})")
-    return im_arr
+    print(f"The shape of image is ({height}, {width}, {len(im.getbands())})")
+    return np.array(im)
