d8e984677afdf7cb2e0288f615019f2a4d039d21
hnur-eris <erisshatice6@gmail.com>
2024-09-28 20:36:34 +0300

    added 01

---
+++ Python-2/ex01/aff_life.py
@@ -0,0 +1,28 @@
+from load_csv import load
+import matplotlib.pyplot as plt
+
+
+def main():
+    """
+        Loads the life expectancy dataset,
+        filters the data for Turkey,
+        and plots the life expectancy over the years.
+    """
+    dataset = load("life_expectancy_years.csv")
+    if dataset is not None:
+        data = dataset[dataset['country'] == 'Turkey']
+        years = data.columns[1:]
+        life_expectancy = data.values[0][1:]
+        plt.plot(years, life_expectancy, label='Turkey')
+        plt.title('Life Expectancy in Turkey Over the Years')
+        plt.xlabel('Year')
+        plt.xticks(years[::40])  # choosing every 40. elements
+        plt.ylabel('Life Expectancy')
+        try:
+            plt.show()
+        except (Exception, KeyboardInterrupt) as e:
+            print(f"Error: {e}")
+
+
+if __name__ == "__main__":
+    main()
